bffffa3f52b78ca01a228cd1e2b2ab10b5b64b05
Lenovo <1766984932@qq.com>
2021-01-27 17:35:41 +0800

    SpringMVC HelloWorld!+Service  2021-0127-1735

--- src/main/java/com/javaboy/controller/MyController.java
+++ src/main/java/com/javaboy/controller/MyController.java
@@ -1,14 +1,21 @@
 package com.javaboy.controller;
 
+import com.javaboy.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
+@org.springframework.stereotype.Controller("/hello")
 public class MyController implements Controller {
+    @Autowired
+    HelloService helloService;
+
     public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse res){
+        String hello = helloService.hello("javaboy2");
+        System.out.println(hello);
         ModelAndView mv = new ModelAndView("hello");
         mv.addObject("name", "javaboy");
         return mv;
