5305c6b9f1de16195c8f2f48130f915013b0939f
Lenovo <1766984932@qq.com>
2021-01-27 15:23:39 +0800

    SpringMVC HelloWorld ! 2021-0127-1523

---
+++ src/main/java/com/javaboy/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.javaboy.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class MyController implements Controller {
+    public ModelAndView handleRequest(HttpServletRequest req, HttpServletResponse res){
+        ModelAndView mv = new ModelAndView("hello");
+        mv.addObject("name", "javaboy");
+        return mv;
+    }
+}
