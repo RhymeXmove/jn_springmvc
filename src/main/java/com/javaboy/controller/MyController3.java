3fb46df8d0c45b0bd5ef060d3dc13fb6eb2db8f4
Lenovo <1766984932@qq.com>
2021-01-28 15:49:59 +0800

    SpringMVC Hello3+RequestMapping  2021-0128-1549

--- src/main/java/com/javaboy/controller/MyController3.java
+++ src/main/java/com/javaboy/controller/MyController3.java
@@ -4,10 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
+@RequestMapping("/user")
+//请求窄化
 public class MyController3 {
-    @RequestMapping("/hello3")
+    @RequestMapping(value = {"/hello3","hello3-2"})
+    //多个地址映射到同一个方法
+/*    @RequestMapping(value = "/hello", mehtod = {RequestMethod.GET, RequestMethod.DELETE, RequestMethod.POST, RequestMethod.PUT})
+      指定多个访问方法
+     */
     public ModelAndView hello() {
-        return new ModelAndView("hello3");
+        ModelAndView mv = new ModelAndView("hello3");
+        mv.addObject("username", "Spring");
+        return mv;
     }
 }
--- src/main/webapp/jsp/hello3.jsp
+++ src/main/webapp/jsp/hello3.jsp
@@ -11,6 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-<h1>hello3!</h1>
+<h1>hello ${username}!</h1>
 </body>
 </html>
