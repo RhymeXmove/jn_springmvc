f0f7148b2d07e1396e47ba37935bc88add7826c8
RhymeXmove <61677430+RhymeXmove@users.noreply.github.com>
2021-02-12 15:06:06 +0800

    SpringMVC 11.1 数据回显 + 11.2 @ModelAttribute 2021-0212-1504

--- src/main/java/com/javaboy/controller/StudentController.java
+++ src/main/java/com/javaboy/controller/StudentController.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Controller
@@ -16,7 +19,8 @@ public class StudentController {
     @RequestMapping("/addstudent")
     @ResponseBody
     // 实体类中groups包含指定的校验规则的会被校验并输出校验结果
-    public void addStudent(@Validated(ValidationGroup2.class) Student2 student2, BindingResult
+    // @ModelAttribute("s")注解，给变量定义别名，student用s替代
+    public String addStudent(@ModelAttribute("s") @Validated(ValidationGroup2.class) Student2 student2, BindingResult
                             result) {
         if (result != null) {
             //校验未通过，获取所哟异常信息并展示出来
@@ -25,6 +29,17 @@ public class StudentController {
                 System.out.println(allError.getObjectName()
                         + ":" + allError.getDefaultMessage());
             }
+            return "student";
         }
+        return "hello";
+    }
+
+    //定义全局数据公共部分
+    @ModelAttribute("info")
+    public Map<String, Object> info() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("username", "javaboy");
+        map.put("address", "www.javaboy.org");
+        return map;
     }
 }
--- src/main/webapp/jsp/addstudent.jsp
+++ src/main/webapp/jsp/addstudent.jsp
@@ -15,19 +15,19 @@
     <table>
         <tr>
             <td>学生编号：</td>
-            <td><input type="text" name="id"></td>
+            <td><input type="text" name="id" value="${s.id}"></td>
         </tr>
         <tr>
             <td>学生姓名：</td>
-            <td><input type="text" name="name"></td>
+            <td><input type="text" name="name" value="${s.name}"></td>
         </tr>
         <tr>
             <td>学生邮箱：</td>
-            <td><input type="text" name="email"></td>
+            <td><input type="text" name="email" value="${s.email}"></td>
         </tr>
         <tr>
             <td>学生年龄：</td>
-            <td><input type="text" name="age"></td>
+            <td><input type="text" name="age" value="${s.age}"></td>
         </tr>
         <tr>
             <td colspan="2">
